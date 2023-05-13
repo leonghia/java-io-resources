dedf7a721d4d75de1900b63899114be254705c38
leonghia <leonghiacnn@gmail.com>
2023-05-14 06:41:05 +0700

    submit AppendingToATextFile exercise

---
+++ exercises/AppendingToATextFile/README.md
@@ -0,0 +1,3 @@
+# Appending to a text file
+
+Create a program to ask the user for several sentences (until they just press Enter) and store them in a text file named "sentences.txt". If the file exists, the new content must be appended to its end.
---
+++ exercises/AppendingToATextFile/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Start typing, hit Enter to submit: ");
+        String output = scanner.nextLine();
+
+        FileWriter outputStream = null;
+
+        try {
+            outputStream = new FileWriter("sentences.txt", true);
+            outputStream.write(output);
+        } finally {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+}
